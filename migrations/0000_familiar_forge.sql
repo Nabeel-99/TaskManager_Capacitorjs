d1b31362a38342aaabd60da9caf5af50f393da0e
Nabeel99 <farouknabil810@gmail.com>
2024-05-15 19:57:49 +0200

    updated

--- migrations/0000_familiar_forge.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `tasks` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`title` text,
-	`description` text,
-	`category` text,
-	`priority` text,
-	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
-	`progress` integer DEFAULT 0
-);
